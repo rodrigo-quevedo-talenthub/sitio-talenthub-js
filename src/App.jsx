728e78e4272f64f8eaa5adbd82dbefe04fc5c8fc
Rodrigo Quevedo (TalentHub) <rodrigo.quevedo@talenthubai.com.ar>
2025-08-28 12:17:16 -0300

    feature (componente reutilizable): CardServicio

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import CardServicio from "./components/CardServicio/CardServicio"
 
 
 function App() {
@@ -7,6 +8,19 @@ function App() {
     <>
         {/* test tailwind OK */}
         {/* <h1 className="bg-black text-white font-bold text-8xl">test</h1> */}
+
+        {/* Poner los componentes aca para probarlos */}
+        <CardServicio 
+            imgUrl={"https://talenthubai.com.ar/images/Iconos/Servicios/1.DesarrolloOrganizacional.png"}
+            nombre={"Desarrollo Organizacional"}
+            descripcion={"Creamos organizaciones ágiles, eficientes y con un fuerte sentido de propósito, alineando la estrategia, la estructura y la cultura."}
+        />
+
+        <CardServicio 
+            imgUrl={"https://talenthubai.com.ar/images/Iconos/Servicios/2.DesarrolloTalento.png"}
+            nombre={"Desarrollo del Talento"}
+            descripcion={"Diseñamos programas de capacitación a medida, basados en las necesidades de tu empresa y sus empleados, utilizando metodologías innovadoras."}
+        />
 
 
     </>
