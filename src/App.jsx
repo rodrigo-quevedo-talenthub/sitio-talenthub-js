d64b342a575ed0be051606b61c65fc99238a9df3
Rodrigo Quevedo (TalentHub) <rodrigo.quevedo@talenthubai.com.ar>
2025-08-28 11:48:24 -0300

    chore: Configurar Tailwind CSS.

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,10 @@ function App() {
 
   return (
     <>
-      
+        {/* test tailwind OK */}
+        {/* <h1 className="bg-black text-white font-bold text-8xl">test</h1> */}
+
+
     </>
   )
 }
