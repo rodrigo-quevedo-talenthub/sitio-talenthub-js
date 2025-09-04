8ccbb49d9429948636326b8f0af4d943e153583d
Lu <lucia.sarmiento@talenthubai.com.ar>
2025-09-04 10:55:34 -0300

    Agregada seccion Nuestra Historia

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,34 @@
 import CardServicio from "./components/CardServicio/CardServicio"
 import InicioSection from "./features/inicio/InicioSection"
 import MainLayout from "./layouts/MainLayout"
+import NuestraHistoriaSection from "./features/nuestra_historia/NuestraHistoriaSection.jsx";
+import ComoEmpezamosSection from "./features/como_empezamos/ComoEmpezamosSection.jsx";
 
 
 function App() {
 
 
-  return (
-    <MainLayout>
+    return (
+        <MainLayout>
 
-        <InicioSection />
-        {/* <InicioSection />
-        <InicioSection /> */}
+            <InicioSection/>
+                {/* <InicioSection/>
+                <InicioSection/> */}
 
-    </MainLayout>
-  )
+
+            <NuestraHistoriaSection>
+                {/* <NuestraHistoriaSection/>
+                <NuestraHistoriaSection/> */}
+            </NuestraHistoriaSection>
+
+
+            <ComoEmpezamosSection>
+                {/* <ComoEmpezamosSection/>
+                <ComoEmpezamosSection/> */}
+            </ComoEmpezamosSection>
+
+        </MainLayout>
+    )
 }
 
 export default App
