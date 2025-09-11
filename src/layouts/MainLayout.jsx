0e2f97eb375d8c922032b606acbfa65a934c378e
Rodrigo Quevedo (TalentHub) <rodrigo.quevedo@talenthubai.com.ar>
2025-09-11 12:43:47 -0300

    test cambio de fuente

--- src/layouts/MainLayout.jsx
+++ src/layouts/MainLayout.jsx
@@ -13,7 +13,7 @@ export default function MainLayout({children}){
         <main  
             // En este caso Tailwind no sirve para setear la URL del background, porque Tailwind necesita una URL estatica, y las URL de assets/ no lo son.
             style={{ backgroundImage: `url(${bg})`}}
-            className="bg-cover bg-no-repeat bg-center bg-fixed font-[Heebo,sans-serif] font-extralight"
+            className="bg-cover bg-no-repeat bg-center bg-fixed font-extralight"
         >
             
             {children}
